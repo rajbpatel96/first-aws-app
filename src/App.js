0d98ce974f5a3b5dbb4d5cfb728c320602ddd623
Raj <rajbpatel96@github.com>
2024-09-27 12:20:14 -0400

    First change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Raj's React Project for AWS <br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
